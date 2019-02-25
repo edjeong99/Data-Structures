ba7faf43833ca55eb6a035032b4abeec4bcb29d5
Edward J <edjeong99@gmail.com>
2019-02-25 16:10:26 -0500

    init  + implement queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,23 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
+        self.size += 1
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if self.size > 0:
+            item = self.storage[0]
+            del self.storage[0]
+            self.size -= 1
+        else:
+            item = None
+
+        return item
+
+    def len(self):
+        return self.size
