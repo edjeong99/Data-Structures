b0321a6c21c98d38f3a9e311ba60da510ef14952
Edward J <edjeong99@gmail.com>
2019-02-26 17:29:33 -0500

    heap works

--- heap/heap.py
+++ heap/heap.py
@@ -13,10 +13,8 @@ class Heap:
         self.storage.pop()
 
         index = 0
-        # loop with _sift_down
 
-        while index*2+1 <= len(self.storage) - 1:
-            index = self._sift_down(index)
+        self._sift_down(index)
         return max
 
     def get_max(self):
@@ -38,24 +36,26 @@ class Heap:
                 break
 
     def _sift_down(self, index):
+        # loop until there is no more child Node
+        while index*2+1 <= len(self.storage) - 1:
 
-        # see which children to compare
-        if len(self.storage) < (2*index)+1:  # there is no child
-            return len(self.storage)
-       # both child node exit and right one is bigger
-        elif len(self.storage) > (2*index)+2 and self.storage[(2*index)+2] >= self.storage[(2*index)+1]:
-            child = (2*index)+2
-        else:  # left child is bigger
-            child = (2*index)+1
+            # see which children to compare
+            if len(self.storage) < (2*index)+1:  # there is no child
+                return
+            # both child node exit and right one is bigger
+            elif len(self.storage) > (2*index)+2 and self.storage[(2*index)+2] >= self.storage[(2*index)+1]:
+                child = (2*index)+2
+            else:  # left child is bigger
+                child = (2*index)+1
 
-        # compare with children and swap if needed
-        if self.storage[child] > self.storage[index]:
-            self.storage[index], self.storage[child] = self.storage[child], self.storage[index]
-            return child
+             # compare with children and swap if needed
+            if self.storage[child] > self.storage[index]:
+                self.storage[index], self.storage[child] = self.storage[child], self.storage[index]
 
-        return len(self.storage)
+            index = child
 
 
+'''
 h = Heap()
 h.insert(5)
 print(h.storage)
@@ -69,3 +69,4 @@ h.delete()
 print(h.storage)
 h.delete()
 print(h.storage)
+'''
